6c2c1b39043c335dcb3433d6b296fe818f814fa2
gaultier.large <gaultier.large@diiage.org>
2025-05-12 12:09:45 +0200

    Initialize project with TypeScript, Express, and configuration files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (_, res) => res.send("Hello from TypeScript & Docker!"));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
